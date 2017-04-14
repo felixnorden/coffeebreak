7b24542dcc3ed611437b61aeb1f297333a4be639
felixnorden <felixnorden@gmail.com>
2017-04-14 15:17:51 +0200

    Added animations and functional FAB, now only creation needs to be implemented

--- Application/CoffeeBreak/app/src/main/java/cbstudios/coffeebreak/activity/MainActivity.java
+++ Application/CoffeeBreak/app/src/main/java/cbstudios/coffeebreak/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package cbstudios.coffeebreak.activity;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,7 +8,12 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -19,12 +25,42 @@ import cbstudios.coffeebreak.model.tododatamodule.todolist.IAdvancedTask;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
+    private FloatingActionButton fabAddBtn, fabAdvBtn, fabListBtn;
+    private TextView txtAdvBtn, txtListBtn;
+    //private LinearLayout fabAdvGroup, fabListGroup;
+
+    private boolean isFabOpen = false;
+
+    Animation FabOpen, FabClose, FabRClockwise, FabRAnticlockwise;
+    Animation TxtSlideIn, TxtSlideOut;
+
     private final Model model = new Model();
-    List<IAdvancedTask> tasks = model.getToDoDataModule().getTasksDummy();
+    private final List<IAdvancedTask> tasks = model.getToDoDataModule().getTasksDummy();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up Buttons for adding tasks
+        fabAddBtn = (FloatingActionButton) findViewById(R.id.fab_add_task);
+        fabAdvBtn = (FloatingActionButton) findViewById(R.id.fab_advanced_task);
+        fabListBtn = (FloatingActionButton) findViewById(R.id.fab_list_task);
+
+        txtAdvBtn = (TextView) findViewById(R.id.advanced_text);
+        txtListBtn = (TextView) findViewById(R.id.list_text);
+
+        // Generate animations
+        setAnimations();
+
+
+        fabAddBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startFabAnimation();
+            }
+        });
 
         // Set up RecyclerView for tasks and render each item.
         TasksAdapter taskAdapter = new TasksAdapter(this, tasks);
@@ -42,6 +78,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,4 +101,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void toggleFabState(){
+        isFabOpen = !isFabOpen;
+    }
+
+    private void startFabAnimation(){
+        if(isFabOpen){
+            fabAdvBtn.startAnimation(FabClose);
+            fabListBtn.startAnimation(FabClose);
+            fabAddBtn.startAnimation(FabRAnticlockwise);
+            txtAdvBtn.startAnimation(TxtSlideOut);
+            txtListBtn.startAnimation(TxtSlideOut);
+            fabAdvBtn.setClickable(false);
+            fabListBtn.setClickable(false);
+            txtAdvBtn.setVisibility(View.INVISIBLE);
+            txtListBtn.setVisibility(View.INVISIBLE);
+
+            toggleFabState();
+        }
+        else{
+            fabAdvBtn.startAnimation(FabOpen);
+            fabListBtn.startAnimation(FabOpen);
+            fabAddBtn.startAnimation(FabRClockwise);
+            txtAdvBtn.startAnimation(TxtSlideIn);
+            txtListBtn.startAnimation(TxtSlideIn);
+            fabAdvBtn.setClickable(true);
+            fabListBtn.setClickable(true);
+            txtAdvBtn.setVisibility(View.VISIBLE);
+            txtListBtn.setVisibility(View.VISIBLE);
+
+            toggleFabState();
+        }
+    }
+
+    private void setAnimations() {
+        FabOpen = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        FabClose = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        FabRClockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_clockwise);
+        FabRAnticlockwise = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_anticlockwise);
+        TxtSlideIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_in_right);
+        TxtSlideOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_out_right);
+    }
+
 }
