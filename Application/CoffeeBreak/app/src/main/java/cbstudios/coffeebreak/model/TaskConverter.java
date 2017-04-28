a75ffdd2e6a4b9aae3b026719ee2238357981115
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2017-04-28 12:09:10 +0200

    Created branch Storage_Alt_1

---
+++ Application/CoffeeBreak/app/src/main/java/cbstudios/coffeebreak/model/TaskConverter.java
@@ -0,0 +1,235 @@
+package cbstudios.coffeebreak.model;
+
+import com.google.gson.*;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import cbstudios.coffeebreak.model.tododatamodule.categorylist.CategoryFactory;
+import cbstudios.coffeebreak.model.tododatamodule.categorylist.ILabelCategory;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.AdvancedTask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.IAdvancedTask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.ITask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.ListTask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.TaskFactory;
+
+/**
+ * Class responsible for converting tasks into different datastructures.
+ */
+
+public class TaskConverter {
+
+    private static TaskConverter INSTANCE = new TaskConverter();
+
+    /**
+     * Fetches the singleton instance of the TaskConverter
+     *
+     * @return the instance of the TaskConverter singleton
+     */
+    public static TaskConverter getInstance() {
+        return INSTANCE;
+    }
+
+    private TaskConverter() {
+    }
+
+    /**
+     * Converts a list of tasks into a JsonArray.
+     *
+     * @param tasks The list of tasks to be converted.
+     * @return The JsonArray containing the data of the tasks in the list.
+     */
+    public JsonArray listToJsonArray(List<IAdvancedTask> tasks) {
+        JsonArray array = new JsonArray();
+
+        for (IAdvancedTask task : tasks) {
+            if (task instanceof ListTask) {
+                array.add(listTaskToJsonObject((ListTask) task));
+            } else if (task instanceof AdvancedTask) {
+                array.add(advancedTaskToJsonObject(task));
+            }
+        }
+
+        return array;
+    }
+
+    /**
+     * Converts a task inte a JsonObject
+     *
+     * @param task The task to be converted.
+     * @return The JsonObject containing the data of the task.
+     */
+    public JsonObject taskToJsonObject(IAdvancedTask task) {
+        if (task instanceof ListTask) {
+            return listTaskToJsonObject((ListTask) task);
+        } else {
+            return advancedTaskToJsonObject(task);
+        }
+    }
+
+    /**
+     * Converts a JsonArray into a List<IAdvancedTask>
+     *
+     * @param array The JsonArray to be converted.
+     * @return The List of IAdvancedTasks.
+     */
+    public List<IAdvancedTask> jsonArrayToList(JsonArray array) {
+        List<IAdvancedTask> list = new ArrayList<>();
+
+        for (int i = 0; i < array.size(); i++) {
+            JsonObject object = array.get(i).getAsJsonObject();
+            String type = object.get("Type").getAsString();
+
+            if (type.equals("ListTask")) {
+                list.add(jsonObjectToListTask(object));
+            } else if (type.equals("AdvancedTask")) {
+                list.add(jsonObjectToAdvancedTask(object));
+            }
+        }
+
+        return list;
+    }
+
+    /**
+     * Converts a ListTask into a JsonObject.
+     *
+     * @param listTask The task that is to be converted into a JsonObject.
+     * @return The JsonObject generated from the task.
+     */
+    private JsonObject listTaskToJsonObject(ListTask listTask) {
+        JsonObject listTaskObject = advancedTaskToJsonObject(listTask);
+        JsonArray subTaskArray = new JsonArray();
+
+        for (ITask subTask : listTask.getSubtasks()) {
+            JsonObject subTaskObject = new JsonObject();
+            subTaskObject.addProperty("Name", subTask.getName());
+            subTaskObject.addProperty("IsChecked", subTask.isChecked());
+            subTaskObject.addProperty("Created", subTask.getCreationCalendar().getTimeInMillis());
+            subTaskArray.add(subTaskObject);
+        }
+
+        listTaskObject.add("Subtasks", subTaskArray);
+
+        return listTaskObject;
+    }
+
+    /**
+     * Converts a AdvancedTask into a JsonObject.
+     *
+     * @param advancedTask The task that is to be converted into a JsonObject.
+     * @return The JsonObject generated from the task.
+     */
+    private JsonObject advancedTaskToJsonObject(IAdvancedTask advancedTask) {
+        JsonObject taskObject = new JsonObject();
+        JsonArray labelArray = new JsonArray();
+
+        if (advancedTask instanceof ListTask) {
+            taskObject.addProperty("Type", "ListTask");
+        } else {
+            taskObject.addProperty("Type", "AdvancedTask");
+        }
+
+        taskObject.addProperty("Name", advancedTask.getName());
+        taskObject.addProperty("Note", advancedTask.getNote());
+        taskObject.addProperty("IsChecked", advancedTask.isChecked());
+        taskObject.addProperty("Priority", advancedTask.getPriority().toString());
+        if (advancedTask.getDate() != null) {
+            taskObject.addProperty("Date", advancedTask.getDate().getTimeInMillis());
+        }
+        taskObject.addProperty("Created", advancedTask.getCreationCalendar().getTimeInMillis());
+
+        for (ILabelCategory label : advancedTask.getLabels()) {
+            labelArray.add(label.getName());
+        }
+
+        taskObject.add("Labels", labelArray);
+
+        return taskObject;
+    }
+
+    /**
+     * Creates an ListTask from a JsonObject. The created task should yield {@<code>true</code>} when equals()
+     * is performed predecessor to this task (The task that it was saved from)
+     * This method assumes the given JsonObject originated from a ListTask.
+     *
+     * @param object The JsonObject to be converted.
+     * @return The task created.
+     */
+    private ListTask jsonObjectToListTask(JsonObject object) {
+        ListTask task = new ListTask();
+        task.setName(object.get("Name").getAsString());
+        task.setNote(object.get("Note").getAsString());
+        task.setChecked(object.get("IsChecked").getAsBoolean());
+        task.setPriority(Priority.valueOf(object.get("Priority").getAsString()));
+
+        if (object.has("Date")) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(Long.valueOf(object.get("Date").getAsString()));
+            task.setDate(cal);
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(Long.valueOf(object.get("Created").getAsString()));
+        task.setCreationCalendar(cal);
+
+        JsonArray labels = object.getAsJsonArray("Labels");
+
+        for (int i = 0; i < labels.size(); i++) {
+            ILabelCategory category = CategoryFactory.getInstance().createLabelCategory(labels.get(i).getAsString());
+            task.addLabel(category);
+        }
+
+        JsonArray subTasks = object.getAsJsonArray("Subtasks");
+
+        for (int i = 0; i < subTasks.size(); i++) {
+            ITask subTask = TaskFactory.getInstance().createTask();
+            JsonObject subTaskObject = subTasks.get(i).getAsJsonObject();
+            subTask.setName(subTaskObject.get("Name").getAsString());
+            subTask.setChecked(subTaskObject.get("IsChecked").getAsBoolean());
+
+            Calendar subTaskCal = Calendar.getInstance();
+            subTaskCal.setTimeInMillis(Long.valueOf(subTaskObject.get("Created").getAsString()));
+            subTask.setCreationCalendar(subTaskCal);
+
+            task.add(subTask);
+        }
+
+        return task;
+    }
+
+    /**
+     * Creates an AdvancedTask from a JsonObject. The created task should yield {@<code>true</code>} when equals()
+     * is performed predecessor to this task (The task that it was saved from).
+     * This method assumes the given JsonObject originated from a AdvancedTask.
+     *
+     * @param object The JsonObject to be converted.
+     * @return The task created.
+     */
+    private AdvancedTask jsonObjectToAdvancedTask(JsonObject object) {
+        AdvancedTask task = new AdvancedTask();
+        task.setName(object.get("Name").getAsString());
+        task.setNote(object.get("Note").getAsString());
+        task.setChecked(object.get("IsChecked").getAsBoolean());
+        task.setPriority(Priority.valueOf(object.get("Priority").getAsString()));
+
+        if (object.has("Date")) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(Long.valueOf(object.get("Date").getAsString()));
+            task.setDate(cal);
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(Long.valueOf(object.get("Created").getAsString()));
+        task.setCreationCalendar(cal);
+
+        JsonArray labels = object.getAsJsonArray("Labels");
+
+        for (int i = 0; i < labels.size(); i++) {
+            ILabelCategory category = CategoryFactory.getInstance().createLabelCategory(labels.get(i).getAsString());
+            task.addLabel(category);
+        }
+
+        return task;
+    }
+}
---
+++ Application/CoffeeBreak/app/src/test/java/cbstudios/coffeebreak/TaskConverterTest.java
@@ -0,0 +1,229 @@
+package cbstudios.coffeebreak;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import cbstudios.coffeebreak.model.Priority;
+import cbstudios.coffeebreak.model.TaskConverter;
+import cbstudios.coffeebreak.model.tododatamodule.categorylist.CategoryFactory;
+import cbstudios.coffeebreak.model.tododatamodule.categorylist.ILabelCategory;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.AdvancedTask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.IAdvancedTask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.ITask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.ListTask;
+import cbstudios.coffeebreak.model.tododatamodule.todolist.Task;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class TaskConverterTest {
+
+    private TaskConverter converter = TaskConverter.getInstance();
+    private List<IAdvancedTask> list;
+    private JsonArray array;
+
+    private Calendar currentCal;
+    private Calendar threeMonthCal;
+    private List<ITask> subtasks;
+
+
+    @Before
+    public void initList() {
+        list = new ArrayList<>();
+        subtasks = new ArrayList<>();
+
+        currentCal = Calendar.getInstance();
+        threeMonthCal = Calendar.getInstance();
+        threeMonthCal.add(Calendar.MONTH, 3);
+
+        List<ILabelCategory> labelCategories = new ArrayList<>();
+        labelCategories.add(CategoryFactory.getInstance().createLabelCategory("Label"));
+        labelCategories.add(CategoryFactory.getInstance().createLabelCategory("Label2"));
+
+        ITask subTask1 = new Task("Subtask1");
+        ITask subTask2 = new Task("Subtask2");
+        subTask1.setChecked(false);
+        subTask2.setChecked(true);
+        subtasks.add(subTask1);
+        subtasks.add(subTask2);
+
+        AdvancedTask at1 = new AdvancedTask("AdvancedTask1");
+        at1.setDate(currentCal);
+        at1.addLabel(labelCategories.get(0));
+        at1.setChecked(true);
+        at1.setNote("Exempel");
+        at1.setPriority(Priority.THREE);
+
+        AdvancedTask at2 = new AdvancedTask("AdvancedTask2");
+        at2.addLabel(labelCategories.get(0));
+        at2.addLabel(labelCategories.get(1));
+        at2.setChecked(false);
+        at2.setNote("");
+        at2.setPriority(Priority.TWO);
+
+        ListTask lt1 = new ListTask("ListTask1");
+        lt1.setDate(threeMonthCal);
+        lt1.addLabel(labelCategories.get(1));
+        lt1.setChecked(true);
+        lt1.setNote("Lorem ipsum");
+        lt1.setPriority(Priority.ONE);
+        lt1.add(subTask1);
+        lt1.add(subTask2);
+
+        list.add(at1);
+        list.add(at2);
+        list.add(lt1);
+    }
+
+    @Test
+    public void testSingleton() {
+        assertTrue(converter != null);
+    }
+
+    @Test
+    public void testListToJsonArray() {
+        JsonArray array = converter.listToJsonArray(list);
+        assertTrue(array.size() == 3);
+
+        //Extract JsonObjects from array
+        JsonObject one = array.get(0).getAsJsonObject();
+        JsonObject two = array.get(1).getAsJsonObject();
+        JsonObject three = array.get(2).getAsJsonObject();
+
+        //Type
+        assertTrue(one.get("Type").getAsString().equals("AdvancedTask"));
+        assertTrue(two.get("Type").getAsString().equals("AdvancedTask"));
+        assertTrue(three.get("Type").getAsString().equals("ListTask"));
+
+        //Name
+        assertTrue(one.get("Name").getAsString().equals("AdvancedTask1"));
+        assertTrue(two.get("Name").getAsString().equals("AdvancedTask2"));
+        assertTrue(three.get("Name").getAsString().equals("ListTask1"));
+
+        //Note
+        assertTrue(one.get("Note").getAsString().equals("Exempel"));
+        assertTrue(two.get("Note").getAsString().equals(""));
+        assertTrue(three.get("Note").getAsString().equals("Lorem ipsum"));
+
+        //Is checked
+        assertTrue(one.get("IsChecked").getAsBoolean());
+        assertFalse(two.get("IsChecked").getAsBoolean());
+        assertTrue(three.get("IsChecked").getAsBoolean());
+
+        //Priorities
+        assertTrue(one.get("Priority").getAsString().equals("THREE"));
+        assertTrue(two.get("Priority").getAsString().equals("TWO"));
+        assertTrue(three.get("Priority").getAsString().equals("ONE"));
+
+        //Dates
+        assertTrue(one.has("Date"));
+        assertTrue(one.get("Date").getAsString().equals(String.valueOf(currentCal.getTimeInMillis())));
+        assertFalse(two.has("Date"));
+        assertTrue(three.has("Date"));
+        assertTrue(three.get("Date").getAsString().equals(String.valueOf(threeMonthCal.getTimeInMillis())));
+
+        //Created-Date
+        assertTrue(one.get("Created").getAsString().equals(String.valueOf(list.get(0).getCreationCalendar().getTimeInMillis())));
+        assertTrue(two.get("Created").getAsString().equals(String.valueOf(list.get(1).getCreationCalendar().getTimeInMillis())));
+        assertTrue(three.get("Created").getAsString().equals(String.valueOf(list.get(2).getCreationCalendar().getTimeInMillis())));
+
+        //Labels
+        assertTrue(one.get("Labels").getAsJsonArray().size() == 1);
+        assertTrue(one.get("Labels").getAsJsonArray().get(0).getAsString().equals("Label"));
+        assertTrue(two.get("Labels").getAsJsonArray().size() == 2);
+        assertTrue(two.get("Labels").getAsJsonArray().get(0).getAsString().equals("Label"));
+        assertTrue(two.get("Labels").getAsJsonArray().get(1).getAsString().equals("Label2"));
+        assertTrue(three.get("Labels").getAsJsonArray().size() == 1);
+        assertTrue(three.get("Labels").getAsJsonArray().get(0).getAsString().equals("Label2"));
+
+        //SubTasks
+        assertFalse(one.has("Subtasks"));
+        assertFalse(two.has("Subtasks"));
+        assertTrue(three.has("Subtasks"));
+        assertTrue(three.getAsJsonArray("Subtasks").size() == 2);
+
+        JsonArray subtasksArray = three.getAsJsonArray("Subtasks");
+
+        JsonObject subTask1 = subtasksArray.get(0).getAsJsonObject();
+        assertTrue(subTask1.get("Name").getAsString().equals("Subtask1"));
+        assertFalse(subTask1.get("IsChecked").getAsBoolean());
+        assertTrue(subTask1.get("Created").getAsString().equals(String.valueOf(subtasks.get(0).getCreationCalendar().getTimeInMillis())));
+
+        JsonObject subTask2 = subtasksArray.get(1).getAsJsonObject();
+        assertTrue(subTask2.get("Name").getAsString().equals("Subtask2"));
+        assertTrue(subTask2.get("IsChecked").getAsBoolean());
+        assertTrue(subTask2.get("Created").getAsString().equals(String.valueOf(subtasks.get(1).getCreationCalendar().getTimeInMillis())));
+
+    }
+
+    @Test
+    public void testTaskToJsonObject() {
+        JsonObject listTaskObj = converter.taskToJsonObject(list.get(2));
+        JsonObject advancedTaskObj = converter.taskToJsonObject(list.get(0));
+
+        assertNotNull(listTaskObj);
+        assertNotNull(advancedTaskObj);
+
+        //Type
+        assertTrue(advancedTaskObj.get("Type").getAsString().equals("AdvancedTask"));
+        assertTrue(listTaskObj.get("Type").getAsString().equals("ListTask"));
+
+        //Name
+        assertTrue(advancedTaskObj.get("Name").getAsString().equals("AdvancedTask1"));
+        assertTrue(listTaskObj.get("Name").getAsString().equals("ListTask1"));
+
+        //Note
+        assertTrue(advancedTaskObj.get("Note").getAsString().equals("Exempel"));
+        assertTrue(listTaskObj.get("Note").getAsString().equals("Lorem ipsum"));
+
+        //Is checked
+        assertTrue(advancedTaskObj.get("IsChecked").getAsBoolean());
+        assertTrue(listTaskObj.get("IsChecked").getAsBoolean());
+
+        //Priorities
+        assertTrue(advancedTaskObj.get("Priority").getAsString().equals("THREE"));
+        assertTrue(listTaskObj.get("Priority").getAsString().equals("ONE"));
+
+        //Dates
+        assertTrue(advancedTaskObj.has("Date"));
+        assertTrue(advancedTaskObj.get("Date").getAsString().equals(String.valueOf(currentCal.getTimeInMillis())));
+        assertTrue(listTaskObj.has("Date"));
+        assertTrue(listTaskObj.get("Date").getAsString().equals(String.valueOf(threeMonthCal.getTimeInMillis())));
+
+        //Created-Date
+        assertTrue(advancedTaskObj.get("Created").getAsString().equals(String.valueOf(list.get(0).getCreationCalendar().getTimeInMillis())));
+        assertTrue(listTaskObj.get("Created").getAsString().equals(String.valueOf(list.get(2).getCreationCalendar().getTimeInMillis())));
+
+        //Labels
+        assertTrue(advancedTaskObj.get("Labels").getAsJsonArray().size() == 1);
+        assertTrue(advancedTaskObj.get("Labels").getAsJsonArray().get(0).getAsString().equals("Label"));
+        assertTrue(listTaskObj.get("Labels").getAsJsonArray().size() == 1);
+        assertTrue(listTaskObj.get("Labels").getAsJsonArray().get(0).getAsString().equals("Label2"));
+
+        //SubTasks
+        assertFalse(advancedTaskObj.has("Subtasks"));
+        assertTrue(listTaskObj.has("Subtasks"));
+        assertTrue(listTaskObj.getAsJsonArray("Subtasks").size() == 2);
+
+        JsonArray subtasksArray = listTaskObj.getAsJsonArray("Subtasks");
+
+        JsonObject subTask1 = subtasksArray.get(0).getAsJsonObject();
+        assertTrue(subTask1.get("Name").getAsString().equals("Subtask1"));
+        assertFalse(subTask1.get("IsChecked").getAsBoolean());
+        assertTrue(subTask1.get("Created").getAsString().equals(String.valueOf(subtasks.get(0).getCreationCalendar().getTimeInMillis())));
+
+        JsonObject subTask2 = subtasksArray.get(1).getAsJsonObject();
+        assertTrue(subTask2.get("Name").getAsString().equals("Subtask2"));
+        assertTrue(subTask2.get("IsChecked").getAsBoolean());
+        assertTrue(subTask2.get("Created").getAsString().equals(String.valueOf(subtasks.get(1).getCreationCalendar().getTimeInMillis())));
+    }
+}
