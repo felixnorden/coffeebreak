c1e30248be6b5549d89e6df77fddc0c572a3217c
Zack Lundqvist <Zack.lundqvist@hotmail.com>
2017-05-18 16:00:53 +0200

    Added some instanceof checks

--- Application/CoffeeBreak/app/src/main/java/cbstudios/coffeebreak/controller/TaskEditPresenter.java
+++ Application/CoffeeBreak/app/src/main/java/cbstudios/coffeebreak/controller/TaskEditPresenter.java
@@ -14,14 +14,16 @@ import cbstudios.coffeebreak.eventbus.OnResumeEvent;
 import cbstudios.coffeebreak.eventbus.OnStopEvent;
 import cbstudios.coffeebreak.model.tododatamodule.todolist.IAdvancedTask;
 import cbstudios.coffeebreak.view.activity.ITaskEditView;
+import cbstudios.coffeebreak.view.activity.TaskEditActivity;
 
 public class TaskEditPresenter extends BasePresenter implements ITaskEditPresenter {
 
-    private ITaskEditView view;
-    private IAdvancedTask task;
+    private final ITaskEditView view;
+    private final IAdvancedTask task;
 
-    TaskEditPresenter(ITaskEditView taskDetailView) {
+    TaskEditPresenter(ITaskEditView taskDetailView, IAdvancedTask task) {
         this.view = taskDetailView;
+        this.task = task;
         EventBus.getDefault().register(this);
     }
 
@@ -49,7 +51,6 @@ public class TaskEditPresenter extends BasePresenter implements ITaskEditPresent
 
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     public void onEditTaskActivityEvent(EditTaskActivityEvent event) {
-        this.task = event.getTask();
         view.setNameText(task.getName());
         view.setNotificationCalendar(task.getDate());
     }
@@ -57,30 +58,40 @@ public class TaskEditPresenter extends BasePresenter implements ITaskEditPresent
     @Override
     @Subscribe
     public void onCreate(OnCreateEvent event) {
+        if (event.getObject() instanceof TaskEditActivity) {
 
+        }
     }
 
     @Override
     @Subscribe
     public void onPause(OnPauseEvent event) {
+        if (event.getObject() instanceof TaskEditActivity) {
 
+        }
     }
 
     @Override
     @Subscribe
     public void onResume(OnResumeEvent event) {
+        if (event.getObject() instanceof TaskEditActivity) {
 
+        }
     }
 
     @Override
     @Subscribe
     public void onDestroy(OnDestroyEvent event) {
+        if (event.getObject() instanceof TaskEditActivity) {
 
+        }
     }
 
     @Override
     @Subscribe
     public void onStop(OnStopEvent event) {
-
+        if (event.getObject() instanceof TaskEditActivity) {
+            EventBus.getDefault().unregister(this);
+        }
     }
 }
